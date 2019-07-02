f595954f85f67cf09488ef367f600de7b0e9f30e
Micah Sheets <micah@soniceyetec.biz>
2019-07-01 17:14:39 -0700

    Update NewsArticleController.php

--- src/Model/NewsArticleController.php
+++ src/Model/NewsArticleController.php
@@ -5,15 +5,4 @@ namespace micahsheets\khnews\Model;
 use PageController;
 
 class NewsArticleController extends PageController {
-  
-  	function Author()
-	{
-		return $this->Author;
-	}
-
-	function Source()
-	{
-		return $this->Source;
-	}
-  
 }
