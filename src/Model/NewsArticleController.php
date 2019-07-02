a4c4a1b474206ce2cc6cd28d4471b619d2250820
Micah Sheets <micah@soniceyetec.biz>
2019-07-01 17:04:34 -0700

    Update NewsArticleController.php
    Add getter functions to NewsArticleController for Author and Source

--- src/Model/NewsArticleController.php
+++ src/Model/NewsArticleController.php
@@ -5,4 +5,15 @@ namespace micahsheets\khnews\Model;
 use PageController;
 
 class NewsArticleController extends PageController {
+  
+  	function Author()
+	{
+		return $this->Author;
+	}
+
+	function Source()
+	{
+		return $this->Source;
+	}
+  
 }
